47185b75619f05c7dc80ad118d0b57c98d62a3c3
Group1all <89590239+Group1all@users.noreply.github.com>
2022-02-27 18:53:45 -0500

    take-home new

--- JS/Script.js
+++ JS/Script.js
@@ -1,6 +1,6 @@
-console.log(Derartu);
+console.log(" hi this is Derartu");
 // Single line comment
-console.log("Tibs");
+console.log(" my favorite food is Tibs");
 /* this is
     multi -line
    comment */
