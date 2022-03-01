4a2080690bcc0aec3d7299fc3520c4200826a8ab
Group1all <89590239+Group1all@users.noreply.github.com>
2022-02-28 19:45:50 -0500

    Update Script.js
    new change day-2

--- JS/Script.js
+++ JS/Script.js
@@ -1,6 +1,7 @@
-console.log("Derartu");
-// Single line comment
-console.log("Tibs");
-/* this is
-    multi -line
-   comment */
+let b="Supercalifragilisticexpialidocious";
+console.log(b.length);
+  let c=123;
+  console.log(c);
+  let bc=123n
+  console.log(typeof bc);
+  
