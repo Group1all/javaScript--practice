294e264484b384af58c191ac58506c93007d232a
Group1all <89590239+Group1all@users.noreply.github.com>
2022-03-02 06:54:21 -0500

    another way

--- JS/Script.js
+++ JS/Script.js
@@ -4,4 +4,10 @@ console.log(b.length);
   console.log(c);
   let bc=123n
   console.log(typeof bc);
-  
+//   or 
+let mystring = "Supercalifragilisticexpialidocious";
+let anyNumber =167;
+let = anyBigInt = 12n;
+console.log("mystring.length = ",mystring.length);
+console.log(`anyNumber = `,anyNumber,`this is a ${typeof(anyNumber)}`);
+console.log(`anyBigInt = `,anyBigInt,`this is a ${typeof(anyBigInt)}`);
